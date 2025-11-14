47e683ab57f7f48e807f4e4c26ed62c27d35fe1c
Selim Aynigül <aynigulselim@gmail.com>
2025-11-14 16:07:13 +0300

    first commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿// Encapsulation Example: BadBankAccount.cs
+
+using DesignPatterns.src.OopPrinciples.Encapsulation;
+
+BadBankAccount badAccount = new BadBankAccount();
+badAccount.balance = 1000m; // Direct access to the Balance field
+Console.WriteLine($"Bad Account Balance: {badAccount.balance}");
+
+BankAccount goodAccount = new BankAccount(1000m);
+goodAccount.Deposit(500m);
+goodAccount.Withdraw(200m);
+Console.WriteLine($"Good Account Balance: {goodAccount.GetBalance()}");
